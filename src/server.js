dcf21994ebdb6e6a1e3f0135a3bcddc2ebc78f4e
Prateek Gogia <prateek_gogia@gridle.io>
2018-11-14 20:59:22 +0530

    [ADDED]: Added Endpoint for GraphQL

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const expressGraphQL = require("express-graphql");
 require("dotenv").config();
 
 mongoose.Promise = global.Promise;
@@ -12,6 +13,14 @@ mongoose.connection
   .once("open", () => console.log("Connected to MongoLab instance."))
   .on("error", error => console.log("Error connecting to MongoLab:", error));
 const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen("3000", () => {
   console.log("Hurray! Server is running on PORT 3000");
